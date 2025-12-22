9bc2c006f40a704d652d39993cd1e4c73cfa0a35
Devashish Upreti <devashup68@gmail.com>
2025-12-22 10:41:53 +0545

    merge two linked list

---
+++ LinkedList/LinkedListProblems/Easy/Problems/mergeTwoLinkedlist.py
@@ -0,0 +1,23 @@
+# class Solution:
+#     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         dummy = ListNode(0)
+#         tail = dummy
+
+#         while l1 and l2:
+#             if l1.val <= l2.val:
+#                 tail.next = l1
+#                 l1 = l1.next
+#             else:
+#                 tail.next = l2
+#                 l2 = l2.next
+
+#             tail = tail.next
+
+#         # Attach remaining nodes
+#         if l1:
+#             tail.next = l1
+#         else:
+#             tail.next = l2
+
+#         return dummy.next
+666666666666666667
